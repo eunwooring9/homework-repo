7ccfaf5d7af848963d6a91384ff19093a9e2e677
eunwoo <eunwooring9@gmail.com>
2026-02-24 17:06:52 +0900

    homework

---
+++ 2026-02-24/2-2-3/password.test.js
@@ -0,0 +1,27 @@
+const { isValidPassword } = require("./password");
+
+describe("isValidPassword", () => {
+  describe("정상 케이스", () => {
+    test("모든 조건 만족 / trun 반환", () => {
+      expect(isValidPassword("Password123")).toBe(true);
+    });
+  });
+
+  describe("실패 케이스", () => {
+    test("유효한 비밀번호", () => {
+      expect(isValidPassword("Password123")).toBe(true);
+    });
+
+    test("8자 미만은 실패", () => {
+      expect(isValidPassword("Pass1")).toBe(false);
+    });
+
+    test("영문 없음", () => {
+      expect(isValidPassword("12345678")).toBe(false);
+    });
+
+    test("빈 문자", () => {
+      expect(isValidPassword("")).toBe(false);
+    });
+  });
+});
